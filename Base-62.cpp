e33cebd71fc1e40718c6fc4c5086064aefdcc782
suikanashi <hashikono@outlook.com>
2026-09-14 18:55:39 -0700

    Added the to-do list stuff

---
+++ Base-62.cpp
@@ -0,0 +1,17 @@
+/*//TODO - Hello :>
+- Make an algorithm that detects the base limit converson
+    - Decimal example; 9 -> 10
+    - Hexadecimal; F -> 10
+- You can also make an optional arguement that allows them to input their own set of characters to use
+    - Decimal example; instead of 0-9, they can replace it to be A-J
+- One function should be for conversions
+- Another function for calculations
+    - basic arithmetics to start: + - * /
+    - add more functions??? (not sure arduino could support it)
+    - Maybe convert everthing to decimal or binary and reconvert it after doing the calculation
+- Any other functions you think would help :>
+- Try not to make this into a class
+- Inputs to these functions will most likely begin as a string
+- Functions should also detect for invalid inputs
+
+*/
